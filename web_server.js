97ee3dff7e9fc03b90828b93e54aedf0ebcfb837
ryuto <tun_tnd@i.softbank.jp>
2019-05-14 14:59:32 +0900

    Add web server

---
+++ web_server.js
@@ -0,0 +1,42 @@
+var http = require("http");
+var fs = require('fs');
+
+function getType(_url) {
+  var types = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "image/png",
+    ".svg": "image/svg+xml"
+  }
+  for (var key in types) {
+    if (_url.endsWith(key)) {
+      return types[key];
+    }
+  }
+  return "text/plain";
+}
+
+var server = http.createServer(function (req, res) {
+  var url = "." +(req.url.endsWith("/") ? req.url + "index.html" : req.url);
+  if (fs.existsSync(url)) {
+    fs.readFile(url, (err, data) => {
+      if (!err) {
+        res.writeHead(200, {"Content-Type": getType(url)});
+        res.end(data);
+      } else {
+        res.statusCode = 500;
+        res.end();
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+var port = process.env.PORT || 8080;
+server.listen(port, function() {
+  console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
+
